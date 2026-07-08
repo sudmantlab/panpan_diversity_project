4f470dfe63f4a3880e2d888b1d170205e3a611c2
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2026-07-08 14:35:40 -0700

    Add script to count genome gaps

---
+++ 3.assembly_stats/telomeres_gaps/count_gaps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Count the number of gaps within a genome
+# Outps a per fasta, per chromosome file and a summary file
+#
+
+genomeDir="XXX YYY"
+
+for dir in $genomeDir
+do
+    echo $dir
+    echo "sample,sequence,count" > $(basename $dir)~gaps.csv
+    for x in $dir/*.fasta
+    do
+        bioawk -c fastx -v SAMPLE=$(basename $x .fasta) '{n = split($seq, arr, /N+/); print SAMPLE","$name","n-1}' $x
+    done >> $(basename $dir)~gaps.csv
+
+    awk -v FS="," -v OFS="," 'BEGIN{print "sample,totalCount"} NR>1{counts[$1] += $3} END{for(i in counts){print i,counts[i]}}'  $(basename $dir)~gaps.csv > $(basename $dir)~summary.csv
+    awk -v FS="," -v OFS="," 'BEGIN{print "sample,totalCount"} NR>1{if($2!~/X/ || $2!~/Y/){counts[$1] += $3}} END{for(i in counts){print i,counts[i]}}'  $(basename $dir)~gaps.csv > $(basename $dir)~summary-autosomes.csv
+done
